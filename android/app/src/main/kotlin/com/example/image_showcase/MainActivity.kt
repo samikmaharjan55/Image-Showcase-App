3c73e6cdc204fb1d5aaff0c0863312f9d72111e2
Samik Maharjan <samikmaharjan55@gmail.com>
2023-02-10 12:30:27 +0545

    Image Showcase App

---
+++ android/app/src/main/kotlin/com/example/image_showcase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.image_showcase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Image Showcase',
+      theme: ThemeData(
+        fontFamily: "Lato",
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Colors.red,
+          primary: Colors.blueGrey,
+        ),
+      ),
+    );
+  }
+}
